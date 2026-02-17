dc96162334a66d6408d38dcfd55fbd4154f2aa11
frockett <crockett.d.ford@gmail.com>
2026-02-17 17:18:21 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -42,6 +42,37 @@ func main() {
 					},
 				},
 			},
+			Tools: []openai.ChatCompletionToolUnionParam{
+				// {"type": "function",
+				// 	"function": {
+				// 		"name": "Read",
+				// 		"description": "Read and return the contents of a file",
+				// 		"parameters": {
+				// 			"type": "object",
+				// 			"properties": {
+				// 				"file_path": {
+				// 					"type": "string",
+				// 					"description": "The path of the file to read"
+				// 				}
+				// 			}
+				// 		},
+				// 		"required": ["file_"]
+				// 	}}
+				openai.ChatCompletionFunctionTool(openai.FunctionDefinitionParam{
+					Name: "Read",
+					Description: openai.String("Read and return the contents of a file"),
+					Parameters: openai.FunctionParameters{
+						"type": "object",
+						"properties": map[string]any{
+							"file_path": map[string]any{
+								"type": "string",
+								"description": "The path of the file to read",
+							},
+						},
+						"required": []string{"file_path"},
+					},
+				}),
+			},
 		},
 	)
 	if err != nil {
